37796b6c5d6cdc0164cacedda9bffe10c1a65c6e
Paweu <pawel.matusz94@gmail.com>
2020-08-30 14:46:51 +0200

    Updates

---
+++ precache-manifest.791741d8fc21082ea50300fc084f24ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7edc976ca8abfc93dc77657687fa1865",
+    "url": "/weather-App/index.html"
+  },
+  {
+    "revision": "7c833120be75a1fb59c1",
+    "url": "/weather-App/static/css/main.16fd9c06.chunk.css"
+  },
+  {
+    "revision": "bd0c503eeca46cfe6eac",
+    "url": "/weather-App/static/js/2.d6262d31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-App/static/js/2.d6262d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c833120be75a1fb59c1",
+    "url": "/weather-App/static/js/main.fbf92707.chunk.js"
+  },
+  {
+    "revision": "480c989dc6e450b59038",
+    "url": "/weather-App/static/js/runtime-main.26633b38.js"
+  }
+]);
